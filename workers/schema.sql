69c68d3f042f55b30b4e02b0c7733d1e435417ae
manas <66103885+manasbir@users.noreply.github.com>
2025-02-18 22:08:44 -0500

    refactor: correct foreign key references in schema.sql

--- workers/schema.sql
+++ workers/schema.sql
@@ -13,14 +13,14 @@ CREATE TABLE floors (
 CREATE TABLE rooms (
     room_id PRIMARY KEY,
     building_code VARCHAR(10) REFERENCES buildings(building_code),
-    floor_id VARCHAR(10) REFERENCES floors(id),
+    floor_id VARCHAR(10) REFERENCES floors(floor_id),
     room_number INTEGER NOT NULL,
     UNIQUE (building_code, room_number)
 );
 
 CREATE TABLE time_slots (
     slot_id PRIMARY KEY,
-    room_id INTEGER REFERENCES rooms(id),
+    room_id INTEGER REFERENCES rooms(room_id),
     day VARCHAR(9) NOT NULL,
     start_time TIME NOT NULL,
     end_time TIME NOT NULL,
